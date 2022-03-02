9e08764b28b9f041e630857d5928bdbbb1e4631f
albi-00 <65985109+albi-00@users.noreply.github.com>
2022-03-02 17:50:01 -0300

    commit inicial

---
+++ unidad7.js
@@ -0,0 +1,16 @@
+ 
+var nombre = prompt("Introduzca su nombre:","");
+document.write("<p class='letraJS'>" + nombre + ", en base a tus gustos hemos preparado menús para vos</p>");
+
+var comida = prompt("Introduzca su comida favorita:","");
+document.write("<p class='letraJS'> ⫸  Comida favorita: " + comida + "</p>");
+ 
+var verdura = prompt("Introduzca su verdura favorita:","");
+document.write("<p class='letraJS'> ⫸  Verdura favorita: " + verdura + "</p>");
+
+var fruta = prompt("Introduzca su fruta favorita:","");
+document.write("<p class='letraJS'> ⫸  Fruta favorita: " + fruta + "</p>");
+
+var bebida = prompt("Introduzca su bebida favorita:","");
+document.write("<p class='letraJS'> ⫸  Bebida favorita: " + bebida + "</p>");
+ 
