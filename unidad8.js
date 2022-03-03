fde6683db7b2b46900e9d2fd44221ef7417ce483
albi-00 <65985109+albi-00@users.noreply.github.com>
2022-03-03 17:15:12 -0300

    Unidad8 JS DOM

---
+++ unidad8.js
@@ -0,0 +1,51 @@
+var caja= document.getElementById('contenedor')
+caja.style.background='url(fondonav.jpg)' 
+
+
+var titulo = document.getElementsByTagName('h1')
+titulo[0].style.color ='black'
+titulo[0].style.fontSize = '24px'
+titulo[0].style.textAlign = 'center' 
+titulo[0].style.fontFamily =' Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 'Geneva, Verdana', 'sans-serif' 
+titulo[0].style.color = 'antiquewhite' 
+titulo[0].style.marginTop = '5%' 
+
+var letra = document.getElementsByTagName('a') 
+letra[5].style.background='url(fondonav.jpg)' 
+letra[4].style.background='url(fondonav.jpg)' 
+
+
+var parrafo = document.getElementsByTagName('p')  
+parrafo[6].style.fontFamily =' Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 'Geneva, Verdana', 'sans-serif' 
+parrafo[6].style.color = 'antiquewhite' 
+parrafo[6].style.textAlign = 'center' 
+parrafo[6].style.fontSize = '100%'
+
+ 
+parrafo[7].style.fontFamily =' Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 'Geneva, Verdana', 'sans-serif' 
+parrafo[7].style.color = 'antiquewhite' 
+parrafo[7].style.textAlign = 'center' 
+parrafo[7].style.fontSize = '100%'
+ 
+parrafo[8].style.fontFamily =' Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 'Geneva, Verdana', 'sans-serif' 
+parrafo[8].style.color = 'antiquewhite'
+parrafo[8].style.textAlign = 'center' 
+parrafo[8].style.fontSize = '100%'
+
+
+parrafo[9].style.fontFamily =' Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 'Geneva, Verdana', 'sans-serif' 
+parrafo[9].style.color = 'antiquewhite'
+parrafo[9].style.textAlign = 'center' 
+parrafo[9].style.fontSize = '100%'
+
+parrafo[10].style.fontStyle = 'italic' 
+parrafo[10].style.color = 'antiquewhite'
+parrafo[10].style.textAlign = 'right' 
+parrafo[10].style.fontSize = '150%'
+parrafo[10].style.marginRight = '20%'
+
+
+var listas = document.getElementsByClassName('contenido')
+listas[0].style.background='url(fondonav.jpg)' 
+listas[1].style.background='url(fondonav.jpg)' 
+listas[2].style.background='url(fondonav.jpg)' 
