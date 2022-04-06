d0835c54baaa5c86f44eebba31317557c820ce3a
Encinas Maria del Alba <albieeeeee17@gmail.com>
2022-04-06 18:09:44 -0300

    mejoras diseño

--- unidad8.js
+++ unidad8.js
@@ -3,36 +3,39 @@ caja.style.backgroundColor='orange'
 
 
 var titulo = document.getElementsByTagName('h1')
-titulo[0].style.color ='black'
+titulo[0].style.color ='rgb(255, 255, 255);'
 titulo[0].style.fontSize = '24px'
 titulo[0].style.textAlign = 'center' 
-titulo[0].style.fontFamily =' Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 'Geneva, Verdana', 'sans-serif' 
+titulo[0].style.font='oblique bold 120% cursive'
 titulo[0].style.color = 'antiquewhite' 
+titulo[0].style.backgroundColor='#727271' 
 titulo[0].style.marginTop = '5%' 
- 
+titulo[0].style.borderRadius='30%' 
+
+
  
 var parrafo = document.getElementsByTagName('p')  
-parrafo[12].style.fontFamily =' Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 'Geneva, Verdana', 'sans-serif' 
-parrafo[12].style.color = 'antiquewhite'
-parrafo[12].style.textAlign = 'center' 
-parrafo[12].style.fontSize = '85%'
-parrafo[12].style.marginTop = '20px'
-
-parrafo[13].style.fontFamily =' Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 'Geneva, Verdana', 'sans-serif' 
-parrafo[13].style.color = 'antiquewhite'
-parrafo[13].style.textAlign = 'center' 
-parrafo[13].style.fontSize = '85%'
-parrafo[13].style.marginTop = '20px'
- 
-parrafo[14].style.fontFamily =' Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 'Geneva, Verdana', 'sans-serif' 
-parrafo[14].style.color = 'antiquewhite' 
-parrafo[14].style.textAlign = 'center' 
-parrafo[14].style.fontSize = '85%'
- 
 parrafo[15].style.fontFamily =' Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 'Geneva, Verdana', 'sans-serif' 
 parrafo[15].style.color = 'antiquewhite'
 parrafo[15].style.textAlign = 'center' 
 parrafo[15].style.fontSize = '85%'
+parrafo[15].style.marginTop = '20px'    
+
+parrafo[16].style.fontFamily =' Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 'Geneva, Verdana', 'sans-serif' 
+parrafo[16].style.color = 'antiquewhite'
+parrafo[16].style.textAlign = 'center' 
+parrafo[16].style.fontSize = '85%'
+parrafo[16].style.marginTop = '20px'
+ 
+parrafo[17].style.fontFamily =' Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 'Geneva, Verdana', 'sans-serif' 
+parrafo[17].style.color = 'antiquewhite' 
+parrafo[17].style.textAlign = 'center' 
+parrafo[17].style.fontSize = '85%'
+ 
+parrafo[18].style.fontFamily =' Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 'Geneva, Verdana', 'sans-serif' 
+parrafo[18].style.color = 'antiquewhite'
+parrafo[18].style.textAlign = 'center' 
+parrafo[18].style.fontSize = '85%'
  
 var listas = document.getElementsByClassName('contenido')
 listas[0].style.backgroundColor='#b8bcc4' 
