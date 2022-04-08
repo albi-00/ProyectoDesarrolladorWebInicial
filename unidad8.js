237a6165be72febca066e642934f05c410f4bda3
Encinas Maria del Alba <albieeeeee17@gmail.com>
2022-04-08 12:37:39 -0300

    correcciones

--- unidad8.js
+++ unidad8.js
@@ -6,7 +6,7 @@ var titulo = document.getElementsByTagName('h1')
 titulo[0].style.color ='rgb(255, 255, 255);'
 titulo[0].style.fontSize = '24px'
 titulo[0].style.textAlign = 'center' 
-titulo[0].style.font='oblique bold 120% cursive'
+titulo[0].style.font='oblique bold 80% cursive'
 titulo[0].style.color = 'antiquewhite' 
 titulo[0].style.backgroundColor='#727271' 
 titulo[0].style.marginTop = '5%' 
@@ -18,28 +18,33 @@ var parrafo = document.getElementsByTagName('p')
 parrafo[15].style.fontFamily =' Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 'Geneva, Verdana', 'sans-serif' 
 parrafo[15].style.color = 'antiquewhite'
 parrafo[15].style.textAlign = 'center' 
-parrafo[15].style.fontSize = '85%'
-parrafo[15].style.marginTop = '20px'    
+parrafo[15].style.fontSize = '70%'
+parrafo[15].style.marginTop = '20px' 
+parrafo[15].style.marginBottom='0px'
+
 
 parrafo[16].style.fontFamily =' Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 'Geneva, Verdana', 'sans-serif' 
 parrafo[16].style.color = 'antiquewhite'
 parrafo[16].style.textAlign = 'center' 
-parrafo[16].style.fontSize = '85%'
+parrafo[16].style.fontSize = '70%'
 parrafo[16].style.marginTop = '20px'
+parrafo[16].style.marginBottom='0px'
+
  
 parrafo[17].style.fontFamily =' Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 'Geneva, Verdana', 'sans-serif' 
 parrafo[17].style.color = 'antiquewhite' 
 parrafo[17].style.textAlign = 'center' 
-parrafo[17].style.fontSize = '85%'
- 
+parrafo[17].style.fontSize = '70%'
+parrafo[17].style.marginBottom='0px'
+
 parrafo[18].style.fontFamily =' Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 'Geneva, Verdana', 'sans-serif' 
 parrafo[18].style.color = 'antiquewhite'
 parrafo[18].style.textAlign = 'center' 
-parrafo[18].style.fontSize = '85%'
- 
+parrafo[18].style.fontSize = '70%'
+parrafo[18].style.marginBottom='0px'
+
 var listas = document.getElementsByClassName('contenido')
 listas[0].style.backgroundColor='#b8bcc4' 
 listas[1].style.backgroundColor='#b8bcc4' 
 listas[2].style.backgroundColor='#b8bcc4' 
-
  
